f4d47fd36a9bddc922a2cd7bb609579baccb7541
Lyn <yan252079467@vip.qq.com>
2020-03-30 10:57:14 +0800

    no message

--- app/Service/EsService.php
+++ app/Service/EsService.php
@@ -14,12 +14,18 @@ use Hyperf\Elasticsearch\ClientBuilderFactory;
 class EsService extends BaseService
 {
     /**
+     * 默认主机
+     * @var string
+     */
+    private $_default_host = 'http://127.0.0.1:9200';
+
+    /**
      * 创建客户端
      * @return \Elasticsearch\Client
      */
     public function client(): \Elasticsearch\Client
     {
-        $host = env('ES_HOST', 'http://127.0.0.1:9200');
+        $host = env('ES_HOST', $this->_default_host);
         // 如果在协程环境下创建，则会自动使用协程版的 Handler，非协程环境下无改变
         $builder = make(ClientBuilderFactory::class)->create();
         return $builder->setHosts([$host])->build();
