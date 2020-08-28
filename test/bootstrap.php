483c221abdae408821bc27035c8195d712ade502
Lyn <yan252079467@vip.qq.com>
2020-08-28 14:05:31 +0800

    no message

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -10,9 +10,6 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
 
-ini_set('display_errors', 'on');
-ini_set('display_startup_errors', 'on');
-
 error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
 
@@ -22,6 +19,8 @@ date_default_timezone_set('Asia/Shanghai');
 Swoole\Runtime::enableCoroutine(true);
 
 require BASE_PATH . '/vendor/autoload.php';
+
+Hyperf\Di\ClassLoader::init();
 
 $container = require BASE_PATH . '/config/container.php';
 
